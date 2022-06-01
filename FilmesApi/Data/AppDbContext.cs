3f02281cef7bcb05ed6deed65a4ad28d280caec6
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-06-01 16:52:14 -0300

    feat: criar entidade sessao

--- FilmesApi/Data/AppDbContext.cs
+++ FilmesApi/Data/AppDbContext.cs
@@ -22,19 +22,31 @@ namespace FilmesApi.Data
                 .HasOne(cinema => cinema.Gerente)
                 .WithMany(gerente => gerente.Cinemas)
                 .HasForeignKey(cinema => cinema.GerenteId);
-                /*
-                    Se quiser que um cinema exista sem a obrigatoriedade de um gerente existir usar codigo abaixo
-                    .HasForeignKey(cinema => cinema.GerenteId).IsRequired(false)
+            /*
+                Se quiser que um cinema exista sem a obrigatoriedade de um gerente existir usar codigo abaixo
+                .HasForeignKey(cinema => cinema.GerenteId).IsRequired(false)
 
-                    Para setar que não haja o efeito cascade (ou seja, que se um gerente for deletado, os cinemas relacionados a ele também seriam)
-                    No caso abaixo o delete vai ser restrito, ou seja, se houverem dependencias, nao será possível a deleção        
-                    .OnDelete(DeleteBehavior.Restrict);
-                */
+                Para setar que não haja o efeito cascade (ou seja, que se um gerente for deletado, os cinemas relacionados a ele também seriam)
+                No caso abaixo o delete vai ser restrito, ou seja, se houverem dependencias, nao será possível a deleção        
+                .OnDelete(DeleteBehavior.Restrict);
+            */
+
+            builder.Entity<Sessao>()
+                .HasOne(sessao => sessao.Filme)
+                .WithMany(filme => filme.Sessoes)
+                .HasForeignKey(sessao => sessao.FilmeId);
+
+            builder.Entity<Sessao>()
+                .HasOne(sessao => sessao.Cinema)
+                .WithMany(cinema => cinema.Sessoes)
+                .HasForeignKey(sessao => sessao.CinemaId);
+
         }
 
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Gerente> Gerentes { get; set; }
+        public DbSet<Sessao> Sessoes { get; set; }
     }
 }
--- FilmesApi/Migrations/20220601195030_Adicionando relacionamento entre cinema e filme.Designer.cs
+++ FilmesApi/Migrations/20220601195030_Adicionando relacionamento entre cinema e filme.Designer.cs
@@ -4,14 +4,16 @@ using FilmesApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FilmesApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220601195030_Adicionando relacionamento entre cinema e filme")]
+    partial class Adicionandorelacionamentoentrecinemaefilme
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
