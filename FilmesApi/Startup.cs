a9228332c7f4842593b8f6d7c27d0a0586252047
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-06-01 11:35:05 -0300

    feat: usar lazy properties + jsonignore para mostrar enderecos na consulta de cinema

--- FilmesApi/Startup.cs
+++ FilmesApi/Startup.cs
@@ -31,7 +31,7 @@ namespace FilmesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("CinemaConnection")));
+            services.AddDbContext<AppDbContext>(opts => opts.UseLazyLoadingProxies().UseSqlServer(Configuration.GetConnectionString("CinemaConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
