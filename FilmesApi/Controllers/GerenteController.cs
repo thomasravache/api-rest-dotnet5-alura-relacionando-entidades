62359196771464911e8d53b3d741531bf3c3838c
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-06-01 12:00:39 -0300

    feat: adicionar entidade gerente

---
+++ FilmesApi/Controllers/GerenteController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos.Gerente;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GerenteController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GerenteController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaGerente([FromBody] CreateGerenteDto gerenteDto)
+        {
+            Gerente gerente = _mapper.Map<Gerente>(gerenteDto);
+            _context.Gerentes.Add(gerente);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(RecuperaGerentesPorId), new { Id = gerente.Id }, gerente);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaGerentesPorId(int id)
+        {
+            Gerente gerente = _context.Gerentes.FirstOrDefault(x => x.Id == id);
+            if (gerente != null)
+            {
+                ReadGerenteDto gerenteDto = _mapper.Map<ReadGerenteDto>(gerente);
+
+                return Ok(gerenteDto);
+            }
+
+            return NotFound();
+        }
+    }
+}
