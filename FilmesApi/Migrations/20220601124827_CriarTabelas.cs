5eca8b72dfe4cd2f862d31b20472b2bf39e51fd4
ThomasRavachedosSant <ThomasRavachedosSant@CVT-NM035>
2022-06-01 10:24:54 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Migrations/20220601124827_CriarTabelas.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmesApi.Migrations
+{
+    public partial class CriarTabelas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cinemas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnderecoFK = table.Column<int>(type: "int", nullable: false),
+                    GerenteFK = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cinemas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duracao = table.Column<int>(type: "int", nullable: false),
+                    Diretor = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Genero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClassificacaoEtaria = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cinemas");
+
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
